91d6395b7fa36fe9ab04ecbe32f4561ed1e2c6eb
Michael Burke <michael_burke4@student.uml.edu>
2024-10-03 17:20:22 -0400

    run each stage of compilation on each testfile

--- src/test.py
+++ src/test.py
@@ -1,26 +1,60 @@
 #!/usr/bin/env python3
+import tempfile
 import os
+import subprocess
 import sys
 
-def error(code, msg):
+def error_out(code, msg):
     print(f'{sys.argv[0]}: {msg}', file=sys.stderr)
     exit(code)
 
+def print_error(msg):
+    print(msg, file=sys.stderr)
+
 if len(sys.argv) != 3:
-    error(1, f'Usage: {sys.argv[0]} compiler_bin tests_dir')
+    error(1, f'usage: {sys.argv[0]} compiler_bin tests_dir')
+
+
+def try_remove(file_paths):
+    for p in file_paths:
+        try:
+            os.remove(p)
+            pass
+        except FileNotFoundError:
+            pass
+        except PermissionError:
+            print('Inadequate permissions to delete tempfile {p}')
+        except Exception as e:
+            print(f'Unexpected exception when deleting tempfile(s) {p}: {e}')
 
 
 class Test:
     def __init__(self, program, comp_error, ret):
         self.program = program
-        self.comp_error = error
+        self.comp_error = comp_error
         self.ret = ret
 
-    def __str__(self):
-        return f''
-
     def run(self, compiler_bin_path):
-        print(compiler_bin_path)
+        tf = open("tmpfile", 'w')
+        bc = f'{tf.name}.bc'
+        obj = f'{tf.name}.o'
+        bn = f'{tf.name}-bin'
+
+        tf.write(self.program)
+        tf.close()
+        cmd = [compiler_bin_path, tf.name, '-o', bc]
+        res = subprocess.run(cmd, capture_output=True, text=True)
+        if not res.stdout.startswith('Debug mode enabled'):
+            error_out(1, f'debug mode not enabled in compiler binary {compiler_bin_path}')
+        llc = ['llc', '--filetype=obj', bc, '-o', obj]
+        llc_res = subprocess.run(llc, capture_output=True, text=True)
+
+        link = ['clang', obj, '-o', bn]
+        link_res = subprocess.run(link, capture_output=True, text=True)
+
+        bn_res = subprocess.run(f'./{bn}', capture_output=True, text=True)
+        try_remove([tf.name, bc, obj, bn])
+        return 1
         
 
 def test_from_testfile(open_file):
@@ -32,27 +66,32 @@ def test_from_testfile(open_file):
         match splt[0]:
             case 'comp_err':
                 if len(splt) != 2:
-                    error(1, f'in testfile {f.name}: comp_error directive requires exactly one arg. Got {len(splt)-1}')
+                    print(f'in testfile {f.name}: comp_error directive requires exactly one arg. Got {len(splt)-1}', file=sys.stderr)
                 err = splt[1]
             case 'ret':
                 if len(splt) != 2:
-                    error(1, f'in testfile {f.name}: ret directive requires exactly one arg. Got {len(splt)-1}')
+                    print_error(f'in testfile {f.name}: ret directive requires exactly one arg. Got {len(splt)-1}')
                 try:
                     ret = int(splt[1])
                 except ValueError:
-                    error(1, f'in testfile {f.name}: could not parse supplied arg of ret directive "{splt[1]}" as integer')
+                    print_error(f'in testfile {f.name}: could not parse supplied arg of ret directive "{splt[1]}" as integer')
             case '--TEXT--':
                 for remaining in open_file:
                     program += remaining
                 return Test(program, err, ret)
             case _:
-                error(1, f'in testfile {f.name}: bad directive "{splt[0]}" in test {f.name}')
-    error(1, f'in tesfile {f.name}: missing text section')
+                print_error(f'in testfile {f.name}: bad directive "{splt[0]}" in test {f.name}')
+    print_error(f'in tesfile {f.name}: missing text section')
 
 
 for file in os.listdir(sys.argv[2]):
-    if file.endswith('.swp'):
+    total = 0
+    passed = 0
+    if not file.endswith('.test'):
         continue
     with open(f'{sys.argv[2]}/{file}', 'r') as f:
         t = test_from_testfile(f)
-        t.run(sys.argv[1])
+        try:
+            t.run(sys.argv[1])
+        except PermissionError:
+            print('Couldn\'t create necessary tempfile in /tmp directory. Fix permissions')
