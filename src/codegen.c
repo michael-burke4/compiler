9aaea83e974bf526154da2feb6ae21ea6cbec1a3
Michael Burke <michael_burke4@student.uml.edu>
2024-10-08 15:41:28 -0400

    begin supporting void ptrs

--- src/codegen.c
+++ src/codegen.c
@@ -59,6 +59,11 @@ static LLVMTypeRef to_llvm_type(LLVMModuleRef mod, ast_type *tp)
 	case Y_VOID:
 		return LLVMVoidTypeInContext(ctxt);
 	case Y_POINTER:
+		// TODO: This is a simplified version of what older clang versions do for void
+		// pointers. Latest versions use opaque pointers for everything. I should follow suit!
+		// also TODO: add casting to make void ptrs usable.
+		if (tp->subtype->kind == Y_VOID)
+			return LLVMPointerType(LLVMInt8TypeInContext(ctxt), 0);
 		return LLVMPointerType(to_llvm_type(mod, tp->subtype), 0);
 	case Y_CHAR:
 		return LLVMInt8TypeInContext(ctxt);
